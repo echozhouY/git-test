7c9782d64b54ec817b98263c96d70885a5dd63fe
echozhouY <15021710716@126.com>
2017-12-26 17:02:12 +0800

    test commit-2-11

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ var s = 'develop';
 var cherrPick = 'cherry-pick';
 
 var a1 = 'commit-1-11';
-var b1 = 'commit-2';
+var b1 = 'commit-21';
 var a2 = 'commit-1-2';
 var c1 = 'commit-3';
 var a3 = 'commit-1-3';
